bc016e7de0875ab1700ffb64108f82d13b4f3e90
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-17 16:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+for (var i = 1; i <= 20; i++) {
+    var output = "";
+
+    if (i % 3 === 0) {
+        output += "Fizz";
+    }
+
+    if (i % 5 === 0) {
+        output += "Buzz";
+    }
+
+    // If the output is still an empty string, it means the number is not a multiple of 3 or 5
+    if (output === "") {
+        output = i.toString();
+    }
+
+   
+    alert(output);
+}
